008ce2e28d4936a364707aa4bb00303e0db1c69a
athena272 <guilhermera272@gmail.com>
2025-02-25 17:44:43 -0300

    Create cipher.js

---
+++ 01-caesar-cipher/cipher.js
@@ -0,0 +1,30 @@
+"use strict";
+function shiftCharacter(character, shifts) {
+    const isUpperCase = character >= 'A' && character <= 'Z';
+    const isLowerCase = character >= 'a' && character <= 'z';
+    if (isUpperCase || isLowerCase) {
+        const base = isUpperCase ? 'A'.charCodeAt(0) : 'a'.charCodeAt(0);
+        const newCharCode = ((character.charCodeAt(0) - base + shifts) % 26 + 26) % 26 + base;
+        return String.fromCharCode(newCharCode);
+    }
+    return character;
+}
+function encryptMessage(message, shifts) {
+    return message
+        .split('')
+        .map(character => shiftCharacter(character, shifts))
+        .join('');
+}
+function decryptMessage(message, shifts) {
+    return message
+        .split('')
+        .map(character => shiftCharacter(character, -shifts))
+        .join('');
+}
+// 🔹 Teste:
+const secretMessage = 'Hello, World! Zz Aa';
+const crypteMessage = encryptMessage(secretMessage, 3);
+console.log("🚀 ~ Encrypted Message:", crypteMessage); // "Khoor, Zruog! Cc Dd"
+const originalMessage = decryptMessage(crypteMessage, 3);
+console.log("🚀 ~ Decrypted Message:", originalMessage); // "Hello, World! Zz Aa"
+console.log(encryptMessage("alura", -4));
