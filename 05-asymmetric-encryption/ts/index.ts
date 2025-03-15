ad8313b013fef04706a0addf867b3889b9052b3f
athena272 <guilhermera272@gmail.com>
2025-03-14 22:57:15 -0300

    Update index.ts

--- 05-asymmetric-encryption/ts/index.ts
+++ 05-asymmetric-encryption/ts/index.ts
@@ -1,5 +1,6 @@
-import { generateKeyPairSync } from "crypto";
+import { generateKeyPairSync, publicEncrypt, privateDecrypt } from "crypto";
 
+// 🔑 Generating an RSA key pair
 const { privateKey, publicKey } = generateKeyPairSync('rsa', {
     modulusLength: 2048,
     publicKeyEncoding: {
@@ -13,3 +14,21 @@ const { privateKey, publicKey } = generateKeyPairSync('rsa', {
 })
 console.log("🚀 ~ publicKey:", publicKey)
 console.log("🚀 ~ privateKey:", privateKey)
+
+// 🔒 Encryption
+const encryptedData = publicEncrypt(
+    publicKey,
+    Buffer.from("Super secret message")
+)
+
+console.log("🚀 ~ Encrypted Data:", encryptedData.toString('hex'));
+
+// 📡 Transmission
+
+// 🔓 Decryption
+const decryptedData = privateDecrypt(
+    privateKey,
+    encryptedData
+);
+
+console.log(`Decrypted Data: ${decryptedData.toString('utf-8')}`);
